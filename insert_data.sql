f58b02d8c0f9dd25757ebf12491efc340c8107b7
kovacsrichard8 <156535601+kovacsrichard8@users.noreply.github.com>
2026-06-09 11:55:40 +0200

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -14,99 +14,98 @@ INSERT INTO dbo.Product_Categories (Category_Name)
 VALUES ('Smartphones'), ('Laptop'), ('Gaming'), ('Television'), ('Household Appliances'); 
 
 INSERT INTO dbo.Products (Product_Name, Price, Product_Category_Id)
-VALUES ('iPhone 16 128 Gb', '274000', '1'),
-('Motorola Edge 60 Fusion 128 Gb', '103000', '1'),
-('Google Pixel 10a 256 Gb', '334999', '1'),
-('MacBook Air 13" 2025', '423900', '2'),
-('MacBook Neo 13" 2026', '318900', '2'),
-('Dell Pro 15 Essential', '359990', '2'),
-('Resident Evil Requiem PS5', '28890', '3'),
-('Call of duty: Black Ops 7 PS4', '12890', '3'),
-('Harry Potter Hogwarts Legacy PS5', '18990', '3'),
-('55" TCL 55V6C', '111990', '4'),
-('65" Hisense 65E7Q PRO', '182990', '4'),
-('55" Samsung QE55QN70F', '196990', '4'),
-('AEG L6SE26DE Washing Machine', '161900', '5'),
-('BOSCH SMV4ENX06E Dishwasher', '165990', '5'),
-('GORENJE MO20E1W Microwave', '24990', '5');
+VALUES ('iPhone 16 128 Gb', 274000, 1),
+('Motorola Edge 60 Fusion 128 Gb', 103000, 1),
+('Google Pixel 10a 256 Gb', 334999, 1),
+('MacBook Air 13" 2025', 423900, 2),
+('MacBook Neo 13" 2026', 318900, 2),
+('Dell Pro 15 Essential', 359990, 2),
+('Resident Evil Requiem PS5', 28890, 3),
+('Call of duty: Black Ops 7 PS4', 12890, 3),
+('Harry Potter Hogwarts Legacy PS5', 18990, 3),
+('55" TCL 55V6C', 111990, 4),
+('65" Hisense 65E7Q PRO', 182990, 4),
+('55" Samsung QE55QN70F', 196990, 4),
+('AEG L6SE26DE Washing Machine', 161900, 5),
+('BOSCH SMV4ENX06E Dishwasher', 165990, 5),
+('GORENJE MO20E1W Microwave', 24990, 5);
 
 INSERT INTO dbo.Orders (Order_Status, Customer_Id)
-VALUES ('Pending', '1'), ('Completed', '2'), ('Completed', '3'), ('Completed','4'), ('Pending', '5'), ('Completed', '6'), 
-('Completed', '7'), ('Completed', '8'), ('Completed', '9'), ('Pending', '10'), ('Completed', '2'), ('Completed', '4'), 
-('Pending', '6'), ('Completed', '8'), ('Pending', '10'), ('Completed', '1'), ('Pending', '3'), ('Completed','5'), 
-('Completed', '7'), ('Pending', '9');
+VALUES ('Pending', 1), ('Completed', 2), ('Completed', 3), ('Completed',4), ('Pending', 5), ('Completed', 6), 
+('Completed', 7), ('Completed', 8), ('Completed', 9), ('Pending', 10), ('Completed', 2), ('Completed', 4), 
+('Pending', 6), ('Completed', 8), ('Pending', 10), ('Completed', 1), ('Pending', 3), ('Completed',5), 
+('Completed', 7), ('Pending', 9);
 
 INSERT INTO dbo.Order_Items (Orders_Id, Product_Id, Quantity, Unit_Price)
-VALUES ('1', '2', '2', '103000'), ('1', '1', '1', '274000'), ('2', '4', '1', '423900'), ('3', '7', '2', '28890'), ('4', '13', '2', '161900'), 
-('5', '10', '1', '111990'), ('6', '15', '3', '24990'), ('7', '3', '1', '334999'), ('8', '9', '2', '18990'), ('9', '14', '1', '165990'), ('10', '5', '1', '318900'),
-('11', '12', '1', '196990'), ('12', '2', '1', '103000'), ('13', '8', '2', '12890'), ('14', '6', '1', '359990'), ('15', '11', '1', '182990'), 
-('1', '15', '1', '24990'), ('5', '3', '1', '334999'), ('10', '12', '1', '196990'), ('12', '1', '1', '274000'), ('15', '4', '1', '423900'), 
-('6', '1', '1', '274000'), ('7', '4', '1', '423900'), ('8', '10', '1', '111990'), ('2', '5', '1', '318900'), ('3', '8', '1', '12890'), 
-('4', '15', '2', '24990'), ('13', '9', '1', '18990'), ('14', '1', '1', '274000'), ('15', '13', '1', '161900');
+VALUES (1, 2, 2, 103000), (1, 1, 1, 274000), (2, 4, 1, 423900), (3, 7, 2, 28890), (4, 13, 2, 161900), 
+(5, 10, 1, 111990), (6, 15, 3, 24990), (7, 3, 1, 334999), (8, 9, 2, 18990), (9, 14, 1, 165990), (10, 5, 1, 318900), 
+(11, 12, 1, 196990), (12, 2, 1, 103000), (13, 8, 2, 12890), (14, 6, 1, 359990), (15, 11, 1, 182990), (1, 15, 1, 24990),
+(5, 3, 1, 334999), (10, 12, 1, 196990), (12, 1, 1, 274000), (15, 4, 1, 423900), (6, 1, 1, 274000), (7, 4, 1, 423900),
+(8, 10, 1, 111990), (2, 5, 1, 318900), (3, 8, 1, 12890), (4, 15, 2, 24990), (13, 9, 1, 18990), (14, 1, 1, 274000), (15, 13, 1, 161900);
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Cash', 'Paid', '2026-04-13', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Cash', 'Paid', 2026-04-13, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 1
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Card', 'Paid', '2026-04-22', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Card', 'Paid', 2026-04-22, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 2
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Card', 'Paid', '2026-04-24 18:59:23', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Card', 'Paid', 2026-04-24 18:59:23, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 4
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Card', 'Unpaid', '2026-05-01 12:54:02', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Card', 'Unpaid', 2026-05-01 12:54:02, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 5
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Cash', 'Unpaid', '2026-05-02 09:30:18', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Cash', 'Unpaid', 2026-05-02 09:30:18, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 6
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Cash', 'Unpaid', '2026-05-04 22:48:09', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Cash', 'Unpaid', 2026-05-04 22:48:09, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 7
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Card', 'Paid', '2026-05-04 23:18:22', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Card', 'Paid', 2026-05-04 23:18:22, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 9
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Cash', 'Unpaid', '2026-05-04 23:18:22', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Cash', 'Unpaid', 2026-05-04 23:18:22, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 10
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Cash', 'Paid', '2026-05-05 07:04:40', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Cash', 'Paid', 2026-05-05 07:04:40, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 10
 GROUP BY Orders_Id;
 
 INSERT INTO dbo.Payments (Payment_Method, Payment_Status, Payment_Date, Amount, Orders_Id)
-SELECT 'Cash', 'Unpaid', '2026-05-06 11:07:52', SUM (Quantity*Unit_Price), Orders_Id 
+SELECT 'Cash', 'Unpaid', 2026-05-06 11:07:52, SUM (Quantity*Unit_Price), Orders_Id 
 FROM dbo.Order_Items
 WHERE Orders_Id = 12
 GROUP BY Orders_Id;
 
 UPDATE dbo.Payments 
 SET Orders_Id = 11
-WHERE Payment_Method ='Cash' AND Payment_Status ='Paid' AND Payment_Date = '2026-05-05 07:04:40';
+WHERE Payment_Method ='Cash' AND Payment_Status ='Paid' AND Payment_Date = 2026-05-05 07:04:40;
 
 UPDATE dbo.Orders SET Order_Date = '2026-04-11 11:28:49' WHERE Id = 1;
 
